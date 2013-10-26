2e98f8c1ef05046fd7006979996e14adc872c33a
Jongeun Lee <yomybaby@yomybaby.com>
2013-10-26 13:48:37 +0900

    push 관련 버그 수정

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,9 +3,11 @@ $.index.open();
 var cloudSessionId = Ti.App.Properties.getString('cloudSessionId');
 if(cloudSessionId){
 	Cloud.sessionId = cloudSessionId;
-	var loginController = Alloy.createController('login');
-	loginController.getView().open();
 }else{
 	var loginController = Alloy.createController('login');
 	loginController.getView().open();
 }
+
+$.emailBtn.addEventListener('click', function(e) {
+	alert('아직 안되지롱~요..facebook으로 해주세요.^^');
+});
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -13,8 +13,10 @@ var fbHandler = function(e){
 		        var user = e.users[0];
 		        Ti.App.Properties.setString('cloudSessionId', Cloud.sessionId);
 		       // alert(String.format('%s님 반갑습니다.',user.last_name+user.first_name));
-		        subscribePushChannel();
-		        //currentWindow.close();
+		        subscribePushChannel(function(){
+		        	currentWindow.close();
+		        });
+		        //
 		    } else {
 		        alert('Error:\n' +
 		            ((e.error && e.message) || JSON.stringify(e)));
@@ -38,19 +40,20 @@ currentWindow.addEventListener('close', function(e) {
 
 
 	
-function subscribePushChannel(){
+function subscribePushChannel(callback){
 	var token = Ti.App.Properties.getString('deviceToken');
 	if(token){
 		Cloud.PushNotifications.subscribe({
 		    channel: 'quest',
 		    device_token: token,
-		    type: 'android'
+		    type: 'gcm'
 		}, function (e) {
 		    if (e.success) {
-		        alert('Success');
+		        alert('첫번째 미션 회원가입 및 로그인 성공하셨습니다. 당첨 및 추가 이벤트는 푸쉬 알림으로 알려드리니 귀(?) 기울여 주세요.');
 		    } else {
 		        alert('ACS PUSH Error:\n' + JSON.stringify(e));
 		    }
+		    callback && callback();
 		});
 	}
 }
