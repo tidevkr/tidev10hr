8736bd7df347ee135e0edaced527cc3c63897ce2
Daehyeon Shin <dahini@gmail.com>
2013-10-26 17:46:15 +0900

    ..

--- app/controllers/questWindow.js
+++ app/controllers/questWindow.js
@@ -7,8 +7,9 @@ user.on('change', function(){
 	// alert( user.get("external_accounts")[0].external_id );
 	// alert(facebookModule.getUid());
 	$.nameLabel.setText(user.get('first_name') + " " + user.get('last_name'));
-	$.profileImageView.setImage("https://graph.facebook.com/"+ user.get("external_accounts")[0].external_id+"/picture?width=96&height=96");
-
+	$.profileImageView.setImage("https://graph.facebook.com/"
+				+ user.get("external_accounts")[0].external_id
+				+"/picture?width=96&height=96");
 
 });
 user.trigger('change');
